501a561dee5598423b291ded29f00aae0e7c0a7c
Leandro Chaves <65681544+leochabu@users.noreply.github.com>
2025-11-27 19:37:03 +0000

    Adding tests

---
+++ tests/calculadora.test.js
@@ -0,0 +1,12 @@
+// import "Calculadora"
+// import Calculadora from "../models/calculadora";
+
+const Calculadora = require("../models/calculadora");
+const calc = new Calculadora();
+
+test("nome", () => {
+  //cal = new Calculadora()
+  let res = calc.dividir([8,2]);
+
+  expect(res).toBe(4);
+});
