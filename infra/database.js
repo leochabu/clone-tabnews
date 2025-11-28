05b521721bcec12a6c072c43d5547abc39b509be
Leandro Chaves <65681544+leochabu@users.noreply.github.com>
2025-11-28 23:39:07 +0000

    Adjusting .env for docker

--- infra/database.js
+++ infra/database.js
@@ -1,9 +1,20 @@
-import { Client, Client } from "pg";
-const client = new Client();
-await client.connect();
-const res = await client.query("SELECT $1::text as message", ["Hello world"]);
-console.log(res.rows[0].message);
-await client.end();
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  client.end();
+
+  return result;
+}
+
 export default {
   query: query,
 };
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,7 @@
 import database from "../../../../infra/database.js";
-function status(request, response) {
-  console.log(database);
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ message: "tudo ok" });
 }
 
