e56963c280db33197a7067c9fbdf07a2eeb30f38
Leandro Chaves <65681544+leochabu@users.noreply.github.com>
2025-11-29 02:36:14 +0000

    Update endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,10 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "tudo ok" });
+  const updated_at = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updated_at,
+  });
 }
 
 export default status;
